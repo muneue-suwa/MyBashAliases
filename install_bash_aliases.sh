fd57d0af1f54fc8ec33d1a86d1a256c7c91b30f5
Dongsik <dongsiku+git@gmail.com>
2019-12-08 21:57:15 +0900

    Fix bug: filename, dot, and source command

--- install_bash_aliases.sh
+++ install_bash_aliases.sh
@@ -3,8 +3,6 @@
 INSTALL_SH_FILENAME=`readlink -f $0`
 INSTALL_SH_DIRNAME=`dirname $INSTALL_SH_FILENAME`
 
-echo "if [ -f $INSTALL_SH_DIRNAME/bash_aliases ]; then" > $HOME/.bash_aliases
-echo "    $INSTALL_SH_DIRNAME/bash_aliases" >> $HOME/.bash_aliases
+echo "if [ -f $INSTALL_SH_DIRNAME/bash_aliases.sh ]; then" > $HOME/.bash_aliases
+echo "  . $INSTALL_SH_DIRNAME/bash_aliases.sh" >> $HOME/.bash_aliases
 echo "fi" >> $HOME/.bash_aliases
-
-source $HOME/.bash_aliases
