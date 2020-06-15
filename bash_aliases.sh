9d9ae1301f4279100e8c12649aedd9dac38084d7
Dongsik <dongsiku@s.okayama-u.ac.jp>
2020-06-15 19:49:46 +0900

    Update check-sha256 with updated app name

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -33,7 +33,7 @@ function cpb(){
 alias cpb=cpb
 
 # My Applications
-alias check-sha256='python3 ~/Applications/verify_linux_iso/main.py'
+alias check-sha256='python3 ~/Applications/sha-2-verifier/sha-2-verifier.py'
 
 # Add PATH
 export PATH="$PATH:$HOME/Applications/node-v10.16.3-linux-x64/bin/"
